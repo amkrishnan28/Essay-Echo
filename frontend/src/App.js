28db3e6247c4d29851482dcbbd39108b766cb7b1
amkrishnan28 <99427361+amkrishnan28@users.noreply.github.com>
2024-03-03 12:40:02 -0800

    Added border box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,27 @@
 import React from 'react';
-import myImage from './logo.jpeg'
+import myImage from './logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
       <section className="App-content">
-        <div className="left-half">
-        <img src={myImage} alt="My Image" className="logo" />
-          <h2>About</h2>
-          <p>
+        <div className="content">
+          <img src={myImage} alt="My Image" className="logo" />
+          <div className="login-form">
+            <form class = "box" method = "post">
+              <h1> Login/Create Account </h1>
+              <input type = "username" name = "" placeholder = "Username"></input>
+              <input type = "password" name = "" placeholder = "Password"></input>
+              <input type = "submit" name = "" placeholder = "Login/Create Account"></input>
+
+            </form>
+          </div>
+          
+          <h2 id="about" >About</h2>
+          <p id = "about-text">
             This website is a demonstration of building a basic React application.
           </p>
-          <h2>Features</h2>
-          <ul>
-            <li>Modern React development</li>
-            <li>Scalable and reusable components</li>
-            <li>Easy integration with other libraries and frameworks</li>
-          </ul>
-          <h2>Contact</h2>
-          <p>
-            Feel free to contact us for any inquiries or feedback.
-          </p>
-          <address>
-            Email: example@example.com<br />
-            Phone: +1 234 567 890
-          </address>
-        </div>
-        <div className="right-half">
-          <div>
-            <a href="#about" id="about">About</a>
-          </div>
-          <div>
-            <a href="#features"id="features">Features</a>
-          </div>
         </div>
       </section>
     </div>
