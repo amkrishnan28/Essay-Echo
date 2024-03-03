2a0137ab81072fe0396fffa0c5065e5389f79ebd
amkrishnan28 <99427361+amkrishnan28@users.noreply.github.com>
2024-03-03 12:44:04 -0800

    Added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,9 +18,15 @@ function App() {
             </form>
           </div>
           
-          <h2 id="about" >About</h2>
+          <h2 id="about" >About Us</h2>
           <p id = "about-text">
-            This website is a demonstration of building a basic React application.
+As fellow college students, we intimately understand the challenges and complexities of navigating the college application journey. We're committed to advocating for equal access to education and getting rid of barriers within the essay writing and college admissions process.
+
+In today's rapidly evolving landscape, access to data, resources, and support is paramount. With college admissions becoming increasingly competitive, the demand for applications to colleges and scholarships is soaring. In fact, according to a survey by Forbes, 29% of all high school students are now applying to 10 or more colleges. Our web application aims to alleviate the burden of this process by providing a platform where individuals can upload their essays and receive a similarity score. Leveraging advanced AI technology, our model identifies common themes across essays, enabling users to group their submissions based on shared prompts or themes.
+
+We recognize the significance of this support for all students, particularly first-generation college students who may face additional challenges in navigating the admissions process. By focusing on the intersections of individual stories and experiences, we strive to empower every applicant to present their authentic voice and narrative.
+
+Join us in uncovering the unique intersections in your story as we work towards a more equitable and inclusive higher education landscape.
           </p>
         </div>
       </section>
