bf6bf0429e4b270b74141ff7a487cfaf0b13b7cd
pravirchugh <pravirchugh@yahoo.com>
2024-03-03 10:55:25 -0800

    Added User and Prompt schema

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+const uri = 'mongodb+srv://pravir:<password>@maincluster.wmp8a5t.mongodb.net/?retryWrites=true&w=majority'
